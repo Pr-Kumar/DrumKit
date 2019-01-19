d193ff2c7a87a9784058d128ebdeb94bd4d96bfc
Prajwal Kumar <prajwalk99@gmail.com>
2019-01-19 10:57:54 -0500

    all the files

---
+++ app/src/main/java/com/example/drumkit/SensorListActivity.java
@@ -0,0 +1,34 @@
+package com.example.drumkit;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import android.app.ListActivity;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class SensorListActivity
+        extends ListActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        List<String> sensorNames = getSensorNames();
+        setContentView(R.layout.sensor_list_view);
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.sensor_list_item, sensorNames));
+    }
+
+    private List<String> getSensorNames() {
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        ArrayList<String> names = new ArrayList<String>();
+        for (Sensor sensor : sensors) {
+            names.add(sensor.getName());
+        }
+        return names;
+    }
+
+}
